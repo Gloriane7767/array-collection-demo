b697e8890aa2b803f595bc5d9e1c3ff140ba1272
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-09 22:46:56 +0100

    add question 3 to 9

--- src/main/java/com/gloriane/ArrayDemo.java
+++ src/main/java/com/gloriane/ArrayDemo.java
@@ -24,6 +24,72 @@ public class ArrayDemo {
         }
         
         System.out.println("======= Ex4 =======");
+        int[] nums = {1, 15, 20};
+        int[] copiedNums = Arrays.copyOf(nums, nums.length);
+        System.out.println("Copied array: " + Arrays.toString(copiedNums));
+
+        System.out.println("======= Ex5 =======");
+        String[][] vals = {{"France", "Paris"}, {"Sweden", "Stockholm"}};
+        for (int i = 0; i < vals.length; i++) {
+            for (int j = 0; j < vals[i].length; j++) {
+                System.out.println(vals[i][j]);
+            }
+        }
+        System.out.println("======= Ex6 =======");
+        int[] arr = {43, 5, 17, 2, 14};
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        double average = (double) sum / arr.length;
+        System.out.println("Average: " + average);
+
+        System.out.println("======= Ex7 =======");
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 9, 12};
+        System.out.println("Even numbers:");
+        for (int num : array) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+        System.out.println("Odd numbers:");
+        for (int num : array) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+
+        System.out.println("======= Ex8 =======");
+        int[] arr1 = {20, 20, 40, 20, 30, 40, 50, 60, 50};
+        int[] unique = new int[arr1.length];
+        int count = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < count; j++) {
+                if (arr1[i] == unique[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                unique[count++] = arr1[i];
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println(unique[i]);
+        }
+
+        System.out.println("======= Ex9 =======");
+        int[] arr2 = {1, 2, 3};
+        arr2 = addElement(arr2, 4);
+        arr2 = addElement(arr2, 5);
+        System.out.println(Arrays.toString(arr2));
+    }
+
+    public static int[] addElement(int[] array, int element) {
+        int[] newArray = Arrays.copyOf(array, array.length + 1);
+        newArray[newArray.length - 1] = element;
+        return newArray;
     }
 
     public static int indexOf(int[] array, int value) {
